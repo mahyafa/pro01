11ddadd56a3db384baffad4124076f8921a81dca
mahyafa <mahyafa@gmail.com>
2024-11-27 05:02:23 -0500

    Add files via upload

---
+++ code01.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pylab as plt
+import numpy as np
+
+# replace "?" to NaN
+df.replace("?", np.nan, inplace = True)
+print(df.columns)
+df.columns.values.tolist()
+missing_data = df.isnull()
+missing_data.head(5)
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print (missing_data[column].value_counts())
+    print("")    
+avg_norm_loss = df["normalized-losses"].astype("float").mean(axis=0)
+print("Average of normalized-losses:", avg_norm_loss)
+df["normalized-losses"].replace(np.nan, avg_norm_loss, inplace=True)
+avg_bore=df['bore'].astype('float').mean(axis=0)
+print("Average of bore:", avg_bore)
+df["bore"].replace(np.nan, avg_bore, inplace=True)
